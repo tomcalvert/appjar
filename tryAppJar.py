f1fc418554b5461e77838684c666058797fac3a2
tom <tomcalvert321@gmail.com>
2017-03-11 15:12:13 +0000

    Add another button option

--- tryAppJar.py
+++ tryAppJar.py
@@ -2,13 +2,15 @@ import sys
 from appJar import gui
 
 def press(button):
- if button == 'llama':
-  print( 'llama pressed!')
- if button == 'tapir':
-  print( 'tapir pressed!')
+	if button == 'llama':
+		print( 'llama pressed!')
+	if button == 'tapir':
+		print( 'tapir pressed!')
+	if button == 'zebra':
+		print( 'zebra pressed!')
  
 
 win = gui()
-win.addButtons(['llama', 'tapir'], press)
+win.addButtons(['llama', 'tapir', 'zebra'], press)
 win.addImage('quadImage', 'quad.jpg')
 win.go()
